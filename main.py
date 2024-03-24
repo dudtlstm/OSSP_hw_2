9369be355a09af8c725384eb3bea64fcf76c6251
dudtlstm <annpys1024@naver.com>
2024-03-24 19:42:01 +0900

    2024-03-24 OSSP 개인과제#2 팀이름 추가

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ name = input("이름을 입력하세요: ")
 # 학번
 student_id = input("학번을 입력하세요: ")
 
+# ossp1 브랜치 - 팀 이름 추가
+team_name = input("팀 이름을 입력하세요: ")
+
 # 입력받은 이름과 학번 출력
 print("\n<출력>")
 print(f"이름: {name}")
 print(f"학번: {student_id}")
+print(f"팀 이름: {team_name}")
